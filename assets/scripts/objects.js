ed4d0aeb7f22ec3231d1d51140653f84f8cd495d
muleya pfano <79614241+pfano-dev@users.noreply.github.com>
2023-04-07 16:08:07 +0200

    Add movie project

---
+++ assets/scripts/objects.js
@@ -0,0 +1,12 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById('search-btn');
+
+const title = document.querySelector("#title").value;
+
+console.log(title)
+
+const addmovieHandler = () =>{
+
+ 
+
+}
